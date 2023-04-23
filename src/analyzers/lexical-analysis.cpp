f0f977dc337c0160e7d10c3e989b168f01e68f0b
Homenique V <homenique.t@gmail.com>
2023-04-23 08:38:42 -0300

    Primeira versão funcional do Lexical analysis

--- src/analyzers/lexical-analysis.cpp
+++ src/analyzers/lexical-analysis.cpp
@@ -1,71 +1,193 @@
-#ifndef ANALYZERS_LEXICAL_ANALYSIS
-#define ANALYZERS_LEXICAL_ANALYSIS
-
 #include <iostream>
 #include <string>
-#include "src/symbols/token.cpp"
-#include "src/symbols/lexeme.cpp"
+#include <vector>
+#include <functional>
+#include <memory>
+#include <algorithm>
+
+#include "../symbols/symbol-table.cpp"
+#include "../symbols/token.cpp"
+#include "../error/l_exception.cpp"
+#include "../symbols/token-type.cpp"
+
+typedef struct StatePackage
+{
+    bool returnChar = false; // deve voltar um char
+    std::string identifier;  // onde o lexima é montado
+} StatePackage;
+
 /**
- * @brief classe base para fazer a represetação de cada estado
- *
+ * @
+ * @brief classe abstrata para geração de estados
  */
 class State
 {
 public:
-    /** Função principal do estado onde cada estado vai 
-     * tratar como deve ser feito a transição para o proximo estado
-     */
-    virtual void handle(char c) = 0;
-    virtual bool isComplete() = 0;
-    virtual State *getNextState() = 0;
-    virtual std::string getLexeme() const = 0;
-};
+    virtual StatePackage handle(char c) = 0;
+    virtual bool isComplete() { return completed; };
+    std::shared_ptr<State> nextState;
 
-/**
- * @brief Representa o primeiro estado do analisador lexico
- */
+protected:
+    bool completed = false;
+};
+// ============================= [HEADER States] ====================================================================================
+// Estado inicial do analisador léxico
 class StartState : public State
 {
-private:
-    std::string lexeme;
-    bool completed;
-    State *nextState = nullptr;
-
 public:
-    // Função principal
-    void handle(char c)
-    {
-      
-    }
-    bool isComplete() override
-    {
-        return completed;
-    }
-    State *getNextState()override{
-        return nextState;
-    }
-    std::string getLexeme(){
-        return lexeme;
-    }
+    StatePackage handle(char c) override;
 };
 
-class LexicalAnalysis
+// ==================================================================================================================================
+class CommentState : public State
+{
+public:
+    StatePackage handle(char c) override
+    {
+        StatePackage package;
+        if (c == '}')
+        {
+            std::cout << "\n voltando para o StartState" << std::endl;
+            nextState = std::make_shared<StartState>();
+        }
+        else
+        {
+            std::cout << c;
+            nextState = std::make_shared<CommentState>();
+        }
+
+        return package;
+    }
+};
+/**
+ *
+ * @brief classe representa o primeiro estado do automato [>s0]
+ */
+
+StatePackage StartState::handle(char c)
+{
+    std::cout << "StartState " << std::endl;
+
+    StatePackage package;
+    if (isAValidUnitarySymbol(c))
+    {
+    }
+    else if (c == '{')
+    {
+        std::cout << "CommentState " << std::endl;
+        nextState = std::make_shared<CommentState>();
+    }
+
+    else if (std::isalpha(c) || c == '_')
+    {
+        if (isItaAlphabetHexa(c))
+        {
+            // chamada para para estado do de possivel valor HEXA
+        }
+        else
+        {
+        }
+    }
+    return package;
+}
+
+/**
+ * @brief Classe principal do Análise léxica
+ */
+class LexerAnalysis
 {
 private:
-    std::string file;
+    std::string file;                    // String a ser tokenizada
+    int file_point;                      // possição atual do char a ser tratado
+    std::shared_ptr<State> currentState; // Validar necessidade
+    SymbolTable *symboltable;
 
 public:
-    LexicalAnalysis(/* args */);
-    ~LexicalAnalysis();
-    Token handleToke();
+    LexerAnalysis(std::string file) : currentState(std::make_shared<StartState>())
+    {
+        this->file = file;
+        file_point = 0;
+        symboltable = new SymbolTable();
+    }
+    /**
+     * @brief função que retorna o proximo possivel token
+     */
+    Token getNextToken()
+    {
+        std::string lexeme = "";
+        char cc;
+        while (currentState->isComplete() || !isEndFile())
+        {
+            cc = getNextChar();
+
+            if (symboltable->isItAValidChar(cc))
+            {
+
+                StatePackage result = currentState->handle(cc);
+
+                if (result.returnChar)
+                {
+                    pushBackCurrentChar();
+                }
+                lexeme += result.identifier;
+                currentState = currentState->nextState;
+            }
+
+            else
+            {
+                std::string str(1, cc);
+
+                throw LException(ErrorCode::INVALIDCHARACTER, file_point, str);
+            }
+        }
+        Token token = Token();
+        // Token t = Token(lexema);
+
+        // adicionar o token
+        // tabela_simbolos.tratar_token(t);
+
+        // Token class
+        // verificar se e eh constante
+        // verificar se e palavra reservada
+        // se nao for nenhum dos dois entao ele eh identificador
+        return Token(TOKEN_ID_ADDITION);
+    };
+
+    void pushBackCurrentChar()
+    {
+        file_point--;
+    }
+
+    char getNextChar()
+    {
+
+        if (isEndFile())
+        {
+            return LEXEME_EOF;
+        }
+
+        return this->file[this->file_point++];
+    }
+
+    bool isEndFile()
+    {
+        return this->file_point >= this->file.size();
+    }
 };
-
-LexicalAnalysis::LexicalAnalysis(/* args */)
+int main(int argc, char const *argv[])
 {
-}
+    try
+    {
 
-LexicalAnalysis::~LexicalAnalysis()
-{
-}
+        LexerAnalysis la("{aabb1111cc}");
 
-#endif
+        la.getNextToken();
+    }
+    catch (const LException &e)
+    {
+
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
