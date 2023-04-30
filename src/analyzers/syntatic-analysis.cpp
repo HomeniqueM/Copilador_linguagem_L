44c74349e76462f6221ed10df8f48bd38c554b56
Guilherme <gui3cosso1pimenta@gmail.com>
2023-04-30 17:24:45 -0300

    fix: correçao da productionA

--- src/analyzers/syntatic-analysis.cpp
+++ src/analyzers/syntatic-analysis.cpp
@@ -275,6 +275,19 @@ void SyntaticAnalysis::productionCMD1()
 void SyntaticAnalysis::productionA()
 {
     matchToken(TOKEN_ID_IDENTIFIER);
+    if(token.getTokenid() == TOKEN_ID_OPEN_BRACKET){
+        matchToken(TOKEN_ID_OPEN_BRACKET);
+        if (token.getTokenid() == TOKEN_ID_IDENTIFIER)
+        {
+            matchToken(TOKEN_ID_IDENTIFIER);
+        }
+        else
+        {
+            matchToken(TOKEN_ID_CONSTANT);
+        }
+
+        matchToken(TOKEN_ID_CLOSE_BRACKET);
+    }
     matchToken(TOKEN_ID_ASSIGNMENT);
     if (token.getTokenid() == TOKEN_ID_READLN)
     {
